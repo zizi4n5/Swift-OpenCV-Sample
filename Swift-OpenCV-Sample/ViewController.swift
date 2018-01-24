be4d553afb8ffbdb02813d815032e644e8acb886
zizi4n5 <zizi4n5@gmail.com>
2018-01-24 21:04:11 +0900

    create Single View App Project

---
+++ Swift-OpenCV-Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-OpenCV-Sample
+//
+//  Created by zizi4n5 on 2018/01/23.
+//  Copyright © 2018年 zizi4n5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
